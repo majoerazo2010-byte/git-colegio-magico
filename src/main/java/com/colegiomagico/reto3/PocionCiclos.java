2c3503117fa6e3cdc23bef406daeee35c684cc83
majoerazo2010-byte <majoerazo2010@gmail.com>
2026-09-05 16:27:01 +0000

    use un ciclo for para que la palabra se repita las veces que el usuario poga

--- src/main/java/com/colegiomagico/reto3/PocionCiclos.java
+++ src/main/java/com/colegiomagico/reto3/PocionCiclos.java
@@ -6,7 +6,12 @@ package com.colegiomagico.reto3;
 public class PocionCiclos {
 
     public static String preparar(int cantidad) {
-        return ""; // TODO: Cambia esto
+        String palabra = "";
+        
+        for (i = 0; i < cantidad ; i++){
+            palabra += "Pocion ";
+        }
+        return palabra; // TODO: Cambia esto
     }
 
     public static void main(String[] args) {
