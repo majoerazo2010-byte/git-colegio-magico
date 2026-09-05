10f6d3efb9577f3bb38f2a26556086a148ce1b59
majoerazo2010-byte <majoerazo2010@gmail.com>
2026-09-05 15:51:46 +0000

    modifique la funcion invocar y agregue el nombre mio y el de mi profesor

--- src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
+++ src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
@@ -6,7 +6,7 @@ package com.colegiomagico.reto1;
 public class HechizoIniciacion {
 
     public static String invocar() {
-        return ""; // TODO: Cambia esto
+        return "Mi nombre es majo erazo y mi profe yeider"; // TODO: Cambia esto
     }
 
     public static void main(String[] args) {
